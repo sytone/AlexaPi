138f23a2b19aca5f059ae432979b09f9f5c30a6d
Sytone <sytone@users.noreply.github.com>
2016-04-08 13:31:27 -0700

    Update initd_alexa.sh

--- initd_alexa.sh
+++ initd_alexa.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+### BEGIN INIT INFO
+# Provides:          alexa
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: alexa
+### END INIT INFO
+
 exec > /var/log/alexa.log 2>&1 
 case "$1" in
 
